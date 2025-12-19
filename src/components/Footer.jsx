99b00a0159da01d1516992f97ac8548afff5526f
Delta Student <dhakadn317@gmail.com>
2025-12-19 13:10:12 +0530

    weather-app using react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import house from '../assets/house.png'
+const Footer = () => {
+  return (
+    <footer class="footer">
+        <img src={house} alt="house image not found" />
+    </footer>
+  )
+}
+
+export default Footer
